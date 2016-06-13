9b3a02026ba5f7872b51bb747648c29c38a6fa02
Fausto Milletari <fausto.milletari@gmail.com>
2016-06-13 20:20:56 +0200

    small update

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ params['ModelParams']=dict()
 #params of the algorithm
 params['ModelParams']['numcontrolpoints']=2
 params['ModelParams']['sigma']=15
-params['ModelParams']['device']=1
+params['ModelParams']['device']=0
 params['ModelParams']['prototxtTrain']=os.path.join(basePath,'Prototxt/train_noPooling_ResNet_cinque.prototxt')
 params['ModelParams']['prototxtTest']=os.path.join(basePath,'Prototxt/test_noPooling_ResNet_cinque.prototxt')
-params['ModelParams']['snapshot']=10500
-params['ModelParams']['dirTrain']=os.path.join(basePath,'PromiseNormalisedOrig/Train')
-params['ModelParams']['dirTest']=os.path.join(basePath,'PromiseNormalised/Test')
+params['ModelParams']['snapshot']=0
+params['ModelParams']['dirTrain']=os.path.join(basePath,'Dataset/Train')
+params['ModelParams']['dirTest']=os.path.join(basePath,'Dataset/Test')
 params['ModelParams']['dirResult']=os.path.join(basePath,'Results') #where we need to save the results (relative to the base path)
-params['ModelParams']['dirSnapshots']=os.path.join(basePath,'Models/MRI_cinque_origDset/') #where to save the models while training
+params['ModelParams']['dirSnapshots']=os.path.join(basePath,'Models/MRI_cinque_snapshots/') #where to save the models while training
 params['ModelParams']['batchsize'] = 2 #the batchsize
 params['ModelParams']['numIterations'] = 100000 #the number of iterations
 params['ModelParams']['baseLR'] = 0.0001 #the learning rate, initial one
@@ -29,7 +29,7 @@ params['ModelParams']['nProc'] = 1 #the number of threads to do data augmentatio
 #params of the DataManager
 params['DataManagerParams']['dstRes'] = np.asarray([1,1,1.5],dtype=float)
 params['DataManagerParams']['VolSize'] = np.asarray([128,128,64],dtype=int)
-params['DataManagerParams']['normDir'] = False
+params['DataManagerParams']['normDir'] = False #if rotates the volume according to its transformation in the mhd file. Not reccommended.
 
 model=VN.VNet(params)
 train = [i for i, j in enumerate(sys.argv) if j == '-train']
