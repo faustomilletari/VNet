f753dd67745db3192e7cdf20298da126f60cb3b8
Fausto Milletari <fausto.milletari@gmail.com>
2016-06-13 19:54:36 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import numpy as np
+import VNet as VN
+
+basePath=os.getcwd()
+
+params = dict()
+params['DataManagerParams']=dict()
+params['ModelParams']=dict()
+
+#params of the algorithm
+params['ModelParams']['numcontrolpoints']=2
+params['ModelParams']['sigma']=15
+params['ModelParams']['device']=1
+params['ModelParams']['prototxtTrain']=os.path.join(basePath,'Prototxt/train_noPooling_ResNet_cinque.prototxt')
+params['ModelParams']['prototxtTest']=os.path.join(basePath,'Prototxt/test_noPooling_ResNet_cinque.prototxt')
+params['ModelParams']['snapshot']=10500
+params['ModelParams']['dirTrain']=os.path.join(basePath,'PromiseNormalisedOrig/Train')
+params['ModelParams']['dirTest']=os.path.join(basePath,'PromiseNormalised/Test')
+params['ModelParams']['dirResult']=os.path.join(basePath,'Results') #where we need to save the results (relative to the base path)
+params['ModelParams']['dirSnapshots']=os.path.join(basePath,'Models/MRI_cinque_origDset/') #where to save the models while training
+params['ModelParams']['batchsize'] = 2 #the batchsize
+params['ModelParams']['numIterations'] = 100000 #the number of iterations
+params['ModelParams']['baseLR'] = 0.0001 #the learning rate, initial one
+params['ModelParams']['nProc'] = 1 #the number of threads to do data augmentation
+
+
+#params of the DataManager
+params['DataManagerParams']['dstRes'] = np.asarray([1,1,1.5],dtype=float)
+params['DataManagerParams']['VolSize'] = np.asarray([128,128,64],dtype=int)
+params['DataManagerParams']['normDir'] = False
+
+model=VN.VNet(params)
+train = [i for i, j in enumerate(sys.argv) if j == '-train']
+if len(train)>0:
+    model.train()
+
+test = [i for i, j in enumerate(sys.argv) if j == '-test']
+if len(test) > 0:
+    model.test()
+
+
